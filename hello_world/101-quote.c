bfc27753947d45015b9efd3632d48eee55536c83
cosmos510 <maximemartin510@gmail.com>
2024-02-19 13:06:19 +0000

    quote

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints it
+* Return: 1 if succes
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
