6d024cfbe41b12b0da6d9b0c4dc91464c1929d9e
cosmos510 <maximemartin510@gmail.com>
2024-02-19 10:46:41 +0000

    hello ,puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
+int main(void)
+{
     /*initialisation char*/
     char Hello_puts[] = "\"Programming is like building a multilingual puzzle";
     puts(Hello_puts);
