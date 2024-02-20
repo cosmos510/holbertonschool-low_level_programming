5a892c4ccb721604545c785025208d747f737d01
cosmos510 <maximemartin510@gmail.com>
2024-02-20 09:21:09 +0000

    putchar alphBet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z';ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar (ch);
 	}
-	for (ch = 'A'; ch <= 'Z';ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar (ch);
 	}
