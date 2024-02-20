98c357d98e61ed6d7bef137747190e2ef8e16cfd
cosmos510 <maximemartin510@gmail.com>
2024-02-20 09:11:02 +0000

    putcar alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,15 @@
 * main - print alphabet
 * Return: 0 if success
 */
-int main (void) {
+int main(void)
+{
 	char ch;
 
-	for(ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-    	putchar(ch);
+		putchar (ch);
 	}
-	putchar('\n');
+	putchar ('\n');
 
-   return(0);
+	return (0);
 }
