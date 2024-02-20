93a849a4df2c3998005cfa7d079bbb8e8a2516bb
cosmos510 <maximemartin510@gmail.com>
2024-02-20 10:17:11 +0000

    pnumbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print number base 10
+* Return: 0 if success
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d",i);
+		i++;
+	}
+
+	return(0);
+}
