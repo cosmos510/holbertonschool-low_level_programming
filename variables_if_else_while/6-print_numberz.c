22ea8f359492103ba046abbff473bdced3b7ab63
cosmos510 <maximemartin510@gmail.com>
2024-02-20 11:10:55 +0000

    print number puthar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print hexa putchar
+* Return: 0 if success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <=57; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+
+	return (0);
+}
