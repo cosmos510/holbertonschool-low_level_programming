7773238b5c07b3c9c0c4f0323cb8eb583b172341
cosmos510 <maximemartin510@gmail.com>
2024-02-20 15:10:34 +0000

    add 0_put

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Print alphabet */
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+	
+    return (0);
+}
