10fb6d44277acbf603486eb14211fc623a009e6a
cosmos510 <maximemartin510@gmail.com>
2024-02-20 12:47:10 +0000

    print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - print number base 16
+* Return: 0 if success
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 48;
+	j = 44;
+	k = 32;
+
+	while (i <= 57)
+	{
+		putchar (i);
+		if (i < 57)
+		{
+			putchar (j);
+			putchar (k);
+		}
+		i++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
