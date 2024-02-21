3352b300786b3bf6b9420ed30772ae0624038872
cosmos510 <maximemartin510@gmail.com>
2024-02-21 14:15:35 +0000

    _abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*  */
+/**
+* _abs - main fonction that prints the alphabet
+* @r: input number
+* Return: r
+*/
+int _abs(int r)
+{
+
+	if (r < 0)
+	{
+		r = r * -1;
+		return(r);
+	}
+	else
+	{
+		return (r);
+	}
+
+}
