f079be142603128e7c37e977500a1bba666e02dc
cosmos510 <maximemartin510@gmail.com>
2024-02-21 12:50:47 +0000

    sign number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/*  */
+/**
+* _print sign - main fonction that prints the alphabet
+* @n: input number
+* Return: 1 if c alphabetic
+*/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	return(2);
+}
