48de3b792b05e1379d7cb1af9c3860def53ee0d1
cosmos510 <maximemartin510@gmail.com>
2024-02-22 15:20:40 +0000

    print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /*  */
 /**
-* add - main fonction that prints the alphabet
+* add - print_to_98 fonction that prints the alphabet
 * @n: input number
 * Return: prints all natural numbers from n to 98, followed by a new line.
 */
 void print_to_98(int n)
 {
-	for(; n < 98; n++)
-		printf("%d, ",n);
+	for (; n < 98; n++)
+		printf("%d, ", n);
 
-	for(; n > 98; n--)
-		printf("%d, ",n);
-	printf("%d\n",n);
+	for (; n > 98; n--)
+		printf("%d, ", n);
+	printf("%d\n", n);
 }
