3186ba5d4e7cd2ab474b892f56fc1bb0fb0f988b
cosmos510 <maximemartin510@gmail.com>
2024-02-22 14:44:37 +0000

    add two number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*  */
+/**
+* add - main fonction that prints the alphabet
+* @a: input number
+* @b: input number
+* Return: some
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
