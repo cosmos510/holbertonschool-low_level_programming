4d13cf4a0e639ab01e0613262464985313235d3d
cosmos510 <maximemartin510@gmail.com>
2024-02-23 12:25:14 +0000

    onction that prints * the sum of all the multiples of 3 or 5 below 1024

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /*  */
 /**
-* print_tim - main fonction that prints the times tables n times
-* @n: input number
+* main - main fonction that prints
+* the sum of all the multiples of 3 or 5 below 1024
+* 
 */
 int main(void)
 {
