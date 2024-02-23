9df3000eea71af095394106a942d09eba227b42c
cosmos510 <maximemartin510@gmail.com>
2024-02-23 16:16:02 +0000

    fibonnaci

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	int i = 0;
 	unsigned long int number1, number2, next_num;
+
 	number2 = 2;
 	number1 = 1;
-	
 	printf("%ld, %ld", number1, number2);
+
 	for (; i < 48; i++)
 		{
 		next_num = number1 + number2;
