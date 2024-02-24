920d317948363571154759b34c52ccc227b3709c
cosmos510 <maximemartin510@gmail.com>
2024-02-24 20:09:28 +0000

    prints the sum of the even-valued terms

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*  */
+/**
+* main - main fonction that prints
+* he sum of the even-valued terms
+* Return: 0 if succes
+*/
+int main(void)
+{
+	unsigned long int number1, number2, sum_even;
+	int i = 0;
+
+	number1 = 1;
+	number2 = 2;
+
+	while (number1 + number2 < 4000000)
+		{
+		number2 += number1;
+		if (number2 % 2 == 0)
+		{
+			sum_even += number2;
+		}
+		number1 = number2 - number1;
+		i++;
+		}
+	printf("%ld\n", sum_even);
+	return (0);
+}
