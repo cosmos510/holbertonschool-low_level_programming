809cdae902e82cf4580e4bf7fd3e4a3f2a3be125
cosmos510 <maximemartin510@gmail.com>
2024-02-26 20:12:24 +0000

    print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* */
+/**
+* print_traingle - main fonction that prints traingle
+* @size: number charactere
+*/
+void print_triangle(int size)
+{
+	int space, display;
+
+	if (size > 0)
+	{
+		for (space = size; space > 0; space--)
+		{
+			for (display = 1; display <= size; display++)
+			{
+				if (display >= space)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+
+
