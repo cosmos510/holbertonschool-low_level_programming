311984dd40936f83753c2a450ec26f0726ecf5ad
cosmos510 <maximemartin510@gmail.com>
2024-02-26 10:08:37 +0000

    multiplies two int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* */
+/**
+* _mul - main fonction that multiplies two integers..
+* @a: input int
+* @b: input int
+* Return: 1 if uppercase
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
