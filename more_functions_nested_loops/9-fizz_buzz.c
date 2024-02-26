d1d34949c5b8bf4bd283783948a38e22625de24a
cosmos510 <maximemartin510@gmail.com>
2024-02-26 13:51:24 +0000

    fizz buz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*  */
+/**
+* main - fizz buzz
+* Return: 0 if succes
+*/
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
