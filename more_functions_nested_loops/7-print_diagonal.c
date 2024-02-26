d5613562be7400e6ffdfaa6ef2cd53370f8ba0eb
cosmos510 <maximemartin510@gmail.com>
2024-02-26 13:24:25 +0000

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* */
+/**
+* print_line - main fonction that draws a straight line in the terminal
+* @n: number charactere
+*/
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n >= 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	if (n == 0)
+		_putchar('\n');
+}
