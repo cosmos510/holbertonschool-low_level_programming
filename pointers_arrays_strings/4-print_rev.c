b0fbbbf09083a633ac24a9aa1ca0b1b8ae63682b
cosmos510 <maximemartin510@gmail.com>
2024-02-27 12:55:31 +0000

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/* */
+/**
+* print_rev - main function that prints a string in reverse
+* @s: number cto update the value
+*/
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len > 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+
+
+}
