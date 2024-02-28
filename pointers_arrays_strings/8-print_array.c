9205a1d18f6a124420f795d5570f0696b63a0b66
cosmos510 <maximemartin510@gmail.com>
2024-02-28 13:02:39 +0000

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_array - main function that prints half of the string
+* @a: array
+* @n number of elements in array
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else
+			printf("%d ,", a[i]);
+	}
+	printf("\n");
+}
