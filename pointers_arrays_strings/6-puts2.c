1ad8b393a96b34ddccf4e6d0a036b00024ce769a
cosmos510 <maximemartin510@gmail.com>
2024-02-28 08:15:59 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* puts2 - main function that prints every other character of a string
+* @str: number to update the value
+*/
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+		len++;
+	}
+	_putchar('\n');
+}
