fb74816658ff6403695016534a440840131b40fa
cosmos510 <maximemartin510@gmail.com>
2024-02-28 13:45:48 +0000

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strcpy - main function that prints half of the string
+* @dest: destination to copy
+* @src: source
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+	return (dest);
+}
