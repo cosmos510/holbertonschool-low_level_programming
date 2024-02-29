3acb7871620c05e5bbf483b93510b193cf657784
cosmos510 <maximemartin510@gmail.com>
2024-02-29 12:58:32 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strcat - main function that concatenates two strings
+* @dest: dest to add to src
+* @src: source
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i <= len; i++)
+	{
+		dest += dest[i] + src[i];
+	}
+	return (dest);
+
+
+}
