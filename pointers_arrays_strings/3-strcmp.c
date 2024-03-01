318355e503ce39eb612744903f5e4702c8a18d04
cosmos510 <maximemartin510@gmail.com>
2024-03-01 10:25:59 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int diff;
 
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
@@ -21,5 +21,5 @@ int _strcmp(char *s1, char *s2)
 			diff = 0;
 		i++;
 	}
-	return(diff);
+	return (diff);
 }
