4281882507205b31ebae967895f18abf7f00b452
cosmos510 <maximemartin510@gmail.com>
2024-03-01 20:13:12 +0000

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,40 +4,24 @@
 * @s: char to change
 * Return: string uppercase
 */
+
 char *cap_string(char *s)
 {
+	char cap[] = {',', ';', '.', '!', '?', '"', '(', ')',
+	'{', '}', ' ', '\t', '\n'};
 	int i = 0;
+	int capcount;
+	int cspc = 13;
 
 	while (s[i])
 	{
-		if (s[i] == ' ' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '\n' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == ',' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == ';' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '.' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '!' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '?' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '"' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '(' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == ')' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '{' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '}' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i] == '\t' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
-		if (s[i +1] == '\n' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] = s[i + 1] - 32;
+		capcount = 0;
+		while (capcount < cspc)
+		{
+			if ((i == 0 || s[i - 1] == cap[capcount]) && (s[i] >= 97 && s[i] <= 122))
+				s[i] = s[i] - 32;
+			capcount++;
+		}
 		i++;
 	}
 	return (s);
