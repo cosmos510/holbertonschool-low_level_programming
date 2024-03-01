64542a6df44fb308ac1d829b9fa05f198966e7a7
cosmos510 <maximemartin510@gmail.com>
2024-03-01 14:31:06 +0000

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* cap_string - main function that capitalizes all words of a string.
+* @s: char to change
+* Return: string uppercase
+*/
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == ' ')
+			s[i + 1] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
