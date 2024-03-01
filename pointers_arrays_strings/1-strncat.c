2d9fcbd132d6dc4b131ae19f40f6f86cd80e18ec
cosmos510 <maximemartin510@gmail.com>
2024-03-01 08:27:13 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[len] != '\0')
 		len++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[len + i] = src[i];
+		dest[len + i] = src[i];
 
 	return (dest);
 }
