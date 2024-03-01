bacf8d5d231c215d4c6fdc88cdcf0dc247e934ca
cosmos510 <maximemartin510@gmail.com>
2024-03-01 15:29:07 +0000

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -34,10 +34,9 @@ char *cap_string(char *s)
 			s[i + 1] = s[i + 1] - 32;
 		if (s[i] == '}' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			s[i + 1] = s[i + 1] - 32;
-		else if (s[i] == '\t' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		if (s[i] == '\t' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			s[i + 1] = s[i + 1] - 32;
 		i++;
 	}
-	
 	return (s);
 }
