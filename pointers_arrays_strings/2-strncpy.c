5d1886e2f70d412a851ef5a6e84efd9777806239
cosmos510 <maximemartin510@gmail.com>
2024-03-01 08:57:28 +0000

    strncopy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strncpy - main function that copy string
+* @dest: destination to copy
+* @src: source
+* @n: number bytes
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i <= n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
