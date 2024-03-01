b7d3e1f34cf6ac3b749e763ff977a72e6fc85dd9
cosmos510 <maximemartin510@gmail.com>
2024-03-01 20:51:23 +0000

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* my_leet - main function that encodes a string into 1337.
+* @s: char to change
+* Return: string uppercase
+*/
+char *leet(char *s)
+{
+	char letter[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char number[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	int i = 0;
+	int count;
+	int num = 10;
+
+	while (s[i])
+	{
+		count = 0;
+		while (count < num)
+		{
+			if (s[i] == letter[count])
+				s[i] = number[count];
+			count++;
+		}
+		i++;
+	}
+	return (s);
+}
