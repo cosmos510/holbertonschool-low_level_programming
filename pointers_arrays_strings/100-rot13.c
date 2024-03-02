163fd6290393299475fd711c2de9c8c4d7e16f46
cosmos510 <maximemartin510@gmail.com>
2024-03-02 18:42:06 +0000

    rot 13

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
-* rot13 - main function that encodes a string using rot13.
+* rot13 - main function that prints half of the string
 * @s: number to update the value
 * Return: char transformed
 */
 char *rot13(char *s)
 {
 	char in[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-	'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-	'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-	'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+			     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
 
 	char out[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-	'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n',
-	'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
-	'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+			     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+				 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
 	int i = 0;
 	int count;
@@ -22,10 +24,15 @@ char *rot13(char *s)
 
 	while (s[i])
 	{
-		for (count = 0; count < num; count++)
+		count = 0;
+		while (count < num)
 		{
 			if (s[i] == in[count])
+			{
 				s[i] = out[count];
+				break;
+			}
+			count++;
 		}
 		i++;
 	}
