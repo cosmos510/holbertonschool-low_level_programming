d3d10eeb869617795873b51dc0fcebb15555767d
cosmos510 <maximemartin510@gmail.com>
2024-03-04 12:44:33 +0000

    add strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* */
+/**
+* _strchr - main function that  fills memory with a constant byte
+* @s: The string in which the character is searched.
+* @c: The character that is searched in the string str
+* Return: part of the input string starting from the first occurrence
+* of the specified character.
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *temp = s;
+
+	while (s[i] != '0')
+	{
+		if (s[i] == c)
+			temp = s -1;
+		s++;
+	}
+	return (temp);
+}
