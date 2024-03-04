feee6a0e64fa5f7774c32134f0e802138ea3303e
cosmos510 <maximemartin510@gmail.com>
2024-03-04 12:31:33 +0000

    push main h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
 #include <unistd.h>
 #include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -24,3 +28,7 @@ char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+
+
+#endif
