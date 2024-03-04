b65d678210ace95df66a529276987260ba6f156e
cosmos510 <maximemartin510@gmail.com>
2024-03-04 10:06:27 +0000

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* */
+/**
+* _memset - main function that  fills memory with a constant byte
+* @s: Starting address of memory to be filled
+* @b: Value to be filled
+* @n: Number of bytes to be filled starting
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,3 +22,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+char *_memset(char *s, char b, unsigned int n);
+
