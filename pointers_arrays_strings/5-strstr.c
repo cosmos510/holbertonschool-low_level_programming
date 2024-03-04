53d9ce0242130fcdd25769480881b75c46afe5e9
cosmos510 <maximemartin510@gmail.com>
2024-03-04 19:55:47 +0000

    add strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* */
+/**
+* _strstr - main function unction finds the first occurrence
+* of the substring needle in the string haystack
+* @haystack: This is the main string to be examined.
+* @needle: This is the sub-string to be searched in string.
+* Return: ointer to the beginning of the located substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack != '\0')
+	{
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (*haystack == *needle)
+				return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
