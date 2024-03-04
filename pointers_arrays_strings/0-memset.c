a821e1b9bd7fed1f5942515cbe60cf8ae55344f3
cosmos510 <maximemartin510@gmail.com>
2024-03-04 10:58:16 +0000

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,17 +5,17 @@
 * @s: Starting address of memory to be filled
 * @b: Value to be filled
 * @n: Number of bytes to be filled starting
-* Return : s after memset
+* Return: s after memset
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *temp = s;
+
 	while (n > 0)
 	{
 		*temp = b;
 		temp++;
 		n--;
 	}
-
 	return (s);
 }
