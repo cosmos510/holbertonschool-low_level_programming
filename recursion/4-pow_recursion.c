06e97ec4eead1423676780e766ae7893beec0e15
cosmos510 <maximemartin510@gmail.com>
2024-03-05 15:07:23 +0000

    add pow recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /* */
 /**
-* pow_recursion - main fonction that hat returns the value
+* _pow_recursion - main fonction that hat returns the value
 * of x raised to the power of y.
 * @x: number to calculate the power of
 * @y: the power of x
