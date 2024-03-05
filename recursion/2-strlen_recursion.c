77b73156fe2b711b382faea7edbc437494ceb41a
cosmos510 <maximemartin510@gmail.com>
2024-03-05 12:36:51 +0000

    strlen recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -7,13 +7,8 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	while (*s)
-	{
-		i += 1;
-		_strlen_recursion(s + 1);
-		s++;
-	}
-	return (i);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
