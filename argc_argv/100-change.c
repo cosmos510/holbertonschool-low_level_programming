6682ace962e5bc4866f66cef3889a818152a9897
cosmos510 <maximemartin510@gmail.com>
2024-03-06 19:55:17 +0000

    add change

---
+++ argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/* */
+/**
+* main - main fonction that prints the minimum number
+* of coins to make change for an amount of money.
+* @argc: number argument
+* @argv: array of pointer argument
+* Return: 0 is all good
+*		 Error is not int passed in argument
+*/
+int main(int argc, char *argv[])
+{
+	int cents;
+	int num_coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		num_coins += 1;
+	}
+	printf("%d\n", num_coins);
+	return(0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -3,9 +3,10 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
-
+int main(int argc, char **argv);
+int main(int argc, char *argv[]);
 
 #endif
