42f5798ac6fdcc45bf24979cfd35b265c39d8e15
cosmos510 <maximemartin510@gmail.com>
2024-03-06 19:06:57 +0000

    add add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,27 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 /* */
 /**
 * main - main fonction that add positive number.
 * @argc: number argument
 * @argv: array of argument
 * Return: 0 is all good
+*		 Error is not int passed in argument
 */
 int main(int argc, char **argv)
 {
-	int i;
+	int i, next;
 	int add = 0;
 
-	if (argc > 2)
+	if (argc > 1)
 	{
-		for (i = 1; i < argc ; i++)
+		for (i = 1; i < argc; i++)
 		{
+			for (next = 0; argv[i][next]; next++)
+				if (argv[i][next] < '0' || argv[i][next] > '9')
+					return (printf("Error\n"));
 			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
 	}
-	if (argc <= 2)
-		printf("Error\n");
 	return (0);
 }
