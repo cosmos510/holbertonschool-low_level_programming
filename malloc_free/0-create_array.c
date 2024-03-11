9a00d99c7ab8c56da99ce5b28325f57be1cfcc11
cosmos510 <maximemartin510@gmail.com>
2024-03-11 19:54:45 +0000

    hot fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,13 +12,13 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	str = malloc(size * sizeof(char));
 
 	if (str == NULL)
 		return (NULL);
-	if (size == 0)
-		return (NULL);
-
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
