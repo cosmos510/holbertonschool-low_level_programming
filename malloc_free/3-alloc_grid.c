29157161e7ea5b8389bc56df1a92f13e8d358104
cosmos510 <maximemartin510@gmail.com>
2024-03-11 16:19:48 +0000

    add alloc grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(width * sizeof(int *));
+		array[i] = malloc(width * sizeof(int));
 
 		if (array[i] == NULL)
 		{
