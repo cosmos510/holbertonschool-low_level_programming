c0922727e0a8e3c1c6016a5744d7d71b5bf532eb
cosmos510 <maximemartin510@gmail.com>
2024-03-11 09:50:41 +0000

    add  create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,8 +18,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+	{
+		if (size == 0)
+			return (NULL);
 		str[i] = c;
-
+	}
 	return (str);
 	free(str);
 }
