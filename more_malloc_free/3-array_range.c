05437154c4180905f267e6afbfd2b1be919062f1
cosmos510 <maximemartin510@gmail.com>
2024-03-12 15:59:33 +0000

    add array range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,12 +15,12 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	size = max - min + 1;
-	array = malloc(size * sizeof(int));
-	size_inv = size - (max + 1);
+	array = malloc(size* sizeof(int));
+	size_inv = 0;
 
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size && size_inv < max; i++)
 		array[i] = size_inv++;
 
 	return (array);
