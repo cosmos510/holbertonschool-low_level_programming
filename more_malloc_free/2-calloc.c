27b49581cb203d82dabb66bb36e55ce2512c73fc
cosmos510 <maximemartin510@gmail.com>
2024-03-12 14:33:06 +0000

    add caloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,12 +1,14 @@
 #include "main.h"
 char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 /* */
 /**
 * _calloc - function that allocates memory for an array,
-* @nmenb: number of elements
+* @nmemb: number of elements
 * @size: size of the elements
 * Return:  pointer to new memory space
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
@@ -25,6 +27,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		_memset(ptr, 0, tot_size);
 	return (ptr);
 }
+
+/**
+* _memset - main function that  fills memory with a constant byte
+* @s: Starting address of memory to be filled
+* @b: Value to be filled
+* @n: Number of bytes to be filled starting
+* Return: s after _memset
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
