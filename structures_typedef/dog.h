08728e82f4912c963fe67e9d5c521a6524a679c0
cosmos510 <maximemartin510@gmail.com>
2024-03-13 10:26:24 +0000

    add dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+/**
+ * struct dog - Structure of dog struc
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
