682c2ec4e3a3f989b7ec2aafb2f5405d7e8d9eeb
cosmos510 <maximemartin510@gmail.com>
2024-03-13 17:30:16 +0000

    add new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+char *_strdup(char *str);
+
 /* */
 /**
 * new_dog - function that create a new dog
@@ -18,10 +20,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ndog);
 		return (NULL);
 	}
-	ndog->name = name;
+	ndog->name = _strdup(name);
 	ndog->age = age;
-	ndog->owner = owner;
+	ndog->owner = _strdup(owner);
 
 	return (ndog);
 
+}
+
+/* */
+/**
+* _strdup - function that returns a pointer to a newly allocated
+* space in memory, which contains a copy of the string given as a parameter.
+* @str: char to duplicate
+* Return:  a pointer to the duplicated string
+*/
+char *_strdup(char *str)
+{
+	int len = 0;
+	int i;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		len += 1;
+
+	s = malloc((len + 1) * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
+	s[i] = '\0';
+	return (s);
 }
