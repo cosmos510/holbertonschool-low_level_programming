b3ed8115bd6b66ef91b71ba20f56b757f62a2337
cosmos510 <maximemartin510@gmail.com>
2024-03-13 17:14:29 +0000

    add new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,20 +3,25 @@
 /**
 * new_dog - function that create a new dog
 * @name: pname of the dog
- * @age: age of the dog
- * @owner: owner's name
+* @age: age of the dog
+* @owner: owner's name
+* Return: new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
+
 	ndog = malloc(sizeof(struct dog));
 
 	if (ndog == NULL)
+	{
+		free(ndog);
 		return (NULL);
+	}
 	ndog->name = name;
 	ndog->age = age;
 	ndog->owner = owner;
 
-	return(ndog);
+	return (ndog);
 
 }
