7db970c784a2e7904d6e53bd60f49c71dfe1e774
cosmos510 <maximemartin510@gmail.com>
2024-03-13 19:03:29 +0000

    add free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/* */
+/**
+* free_dog - function that that frees dogs.
+* @d: dog to free
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
