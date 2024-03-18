a555c9eca228795d4509f6d4ede84d80d405805e
cosmos510 <maximemartin510@gmail.com>
2024-03-18 15:36:15 +0000

    add main c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,16 +9,17 @@
 int main(int argc, char *argv[])
 {
 	char *operation;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-
-	operation = argv[2];
+	int num1, num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = argv[2];
 
 	if (get_op_func(operation) == NULL || operation[1] != '\0')
 	{
