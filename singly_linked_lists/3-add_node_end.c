a7a1986a1c65b4f3bbdc11491de700c03da5bed9
cosmos510 <maximemartin510@gmail.com>
2024-04-02 12:53:11 +0000

    adding one conditon

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	int len;
-	list_t *new_node;
+	list_t *new_node, *last_node;
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
@@ -27,12 +27,13 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = len;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 		*head = new_node;
 	else
 	{
-		list_t *last_node = *head;
+		last_node = *head;
 
 		while (last_node->next != NULL)
 			last_node = last_node->next;
