bb1d09e2ad3eb18d25bb5240eaf1cdace4623cad
cosmos510 <maximemartin510@gmail.com>
2024-04-02 13:26:44 +0000

    add print first

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* startupFunction - add new node at the end of the list
+*/
+void __attribute__ ((constructor)) startupFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
