67e18606cf728fb318b232e2da00c11e013d020e
cosmos510 <maximemartin510@gmail.com>
2024-04-02 11:46:11 +0000

    add add_node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* add_node - that returns the number of elements in a linked list_t list.
+* @head: Pointer to the head of the linked list.
+* @str: char to add at the beggining of the linked list
+* Return: The adress of the new element if no problem
+*         Null if it failed
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	int len;
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
