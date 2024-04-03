3df3f7a809d3aa21d67301196d8d85e572470f5a
cosmos510 <maximemartin510@gmail.com>
2024-04-03 12:46:55 +0000

    fixing error for condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->prev = tmp->prev;
+	new->prev = tmp;
 	new->next = tmp->next;
 	tmp->next->prev = new;
 	tmp->next = new;
