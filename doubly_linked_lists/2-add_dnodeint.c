2b6b9d3733760ff1f7ab38366647f711408406cd
cosmos510 <maximemartin510@gmail.com>
2024-04-03 07:02:11 +0000

    add new_node->prev = NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,12 +14,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
-	{
 		return (NULL);
-		free(new_node);
-	}
 
 	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
 
