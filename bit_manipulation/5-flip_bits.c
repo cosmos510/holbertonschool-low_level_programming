82959c8d7f3c7166eb782707e0582d01f468e1a5
cosmos510 <maximemartin510@gmail.com>
2024-04-04 18:21:15 +0000

    add flip bit

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: number giver
+ * @m: The number to flip n to.
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	unsigned int xor = n ^ m;
+
+	while (xor > 0)
+	{
+		count++;
+		xor &= (xor - 1);
+	}
+	return (count);
+}
