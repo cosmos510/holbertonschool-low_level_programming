8ad4e41e73bd73d53c8a89dd5368419232c81556
cosmos510 <maximemartin510@gmail.com>
2024-04-04 14:15:42 +0000

    add get bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - that returns the value of a bit at a given index.
+ * @n: number to transform to binary
+ * @index: index to return the bit
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	return (n >> index & 1);
+}
