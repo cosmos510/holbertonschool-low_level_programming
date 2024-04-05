283a1006f4df7aa1c17b738e1814053715e3c8d3
cosmos510 <maximemartin510@gmail.com>
2024-04-05 15:17:49 +0000

    changinf file permission

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 
-	o = open(filename, O_CREAT | O_RDWR | O_APPEND, 600);
+	o = open(filename, O_CREAT | O_RDWR | O_APPEND, 0600);
 	n_w = write(o, text_content, len);
 
 	if (o == -1 || n_w == -1)
