758ec4a22992022985f20f01d88b677c984b1eda
cosmos510 <maximemartin510@gmail.com>
2024-04-05 09:05:57 +0000

    adding close

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,14 @@
 #include"main.h"
 
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: name of the file
+ * @letters:  is the number of letters it should read and print
+ * Return: If 0 if problem
+ *         Otherwise - number of letters it could read and print
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -9,26 +18,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename,O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
-		return(0);
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		free(buffer);
-		return(0);
+		return (0);
 	}
 	n_r = read(fd, buffer, letters);
 
 	if (n_r == -1)
+	{
+		close(fd);
 		return (0);
+	}
 
 	n_w = write(1, buffer, letters);
 
 	if (n_w == -1)
-		return(0);
+	{
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
 
 	return (n_r);
 
