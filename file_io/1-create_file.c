6ba4410e3c6f5ba057d2605043f8b14a849b4888
cosmos510 <maximemartin510@gmail.com>
2024-04-05 15:22:58 +0000

    betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return(1);
+		return (1);
 
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
