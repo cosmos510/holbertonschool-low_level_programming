f8ac94497a090a9c57d19bff13dab1831259ee16
cosmos510 <maximemartin510@gmail.com>
2024-04-05 17:10:58 +0000

    condition to close

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -48,5 +48,10 @@ int main(int argc, char **argv)
 		}
 	}
 	close(o), close(to);
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
+		exit(100);
+	}
 	return (0);
 }
