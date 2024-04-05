910b3786491625023089bac47e0a86aa5d053ba1
cosmos510 <maximemartin510@gmail.com>
2024-04-05 15:19:56 +0000

    changing trunc istead of append

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,15 +18,14 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 
-	o = open(filename, O_CREAT | O_RDWR | O_APPEND, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	n_w = write(o, text_content, len);
 
 	if (o == -1 || n_w == -1)
 	{
 		close(o);
-		return(-1);
+		return (-1);
 	}
-
 	close(o);
-	return(1);
+	return (1);
 }
